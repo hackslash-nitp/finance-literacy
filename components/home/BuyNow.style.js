04e637b357c780954a92d19d20ac905eedc0acde
Amisha <bamisha814@gmail.com>
2021-10-29 11:36:04 +0530

    change css to styled component

---
+++ components/home/BuyNow.style.js
@@ -0,0 +1,41 @@
+import styled from 'styled-components';
+
+export const Flexitem = styled.div`
+    display: flex;
+`;
+export const Content1 = styled.div`
+position: absolute;
+width: 552px;
+height: 108px;
+left: 89px;
+top: 147.1px;
+font-family: Poppins;
+font-style: normal;
+font-weight: 500;
+font-size: 36px;
+line-height: 54px;
+color: #000000;
+`;
+export const Content2 = styled.div`
+position: absolute;
+width: 510px;
+height: 180px;
+left: 90px;
+top: 303.1px;
+
+font-family: Poppins;
+font-style: normal;
+font-weight: normal;
+font-size: 24px;
+line-height: 36px;
+
+color: #676767;
+`;
+export const Container2 = styled.div`
+    position: absolute;
+    width: 571px;
+    height: 446px;
+    left: 777px;
+    top: 121.1px;
+    right:92;
+ `;
--- components/home/buyNow.js
+++ components/home/buyNow.js
@@ -1,23 +1,18 @@
-import buynow from "../../styles/buynow.module.css" 
+import {Flexitem,Content1,Content2,Container2} from './BuyNow.style';
 import Image from "next/image"
-
-function buyNow (){
+function buyNow() {
     return(
-      <div className={buynow.flexitem1}>
-      
-      <div className={buynow.content1}>
+    <Flexitem>
+    <Content1>
         <p>Real time updates and performance demographics</p>
-      </div>
-      <div className={buynow.content2}>
+    </Content1>
+    <Content2>
         <p>I do not know what to type here so let me fill some random text. Lorem ipsum is hard to consume and I use it as a last resort. So, I will fill random text please and you do not even have to read it.</p>
-      </div>
-      
-      <div className={buynow.container2}>
-         <Image src="/feature.jpg" alt="" width={571} height={446}/>
-      </div>
-      </div>
-    
-    )
+    </Content2>
+    <Container2>
+        <Image src="/feature.jpg" alt="" width={571} height={446} />
+    </Container2>
+    </Flexitem>
+    );
 }
-
 export default buyNow;
