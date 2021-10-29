6504bbe570e9442d49670b3393654281dff7743f
Amisha <bamisha814@gmail.com>
2021-10-29 20:12:46 +0530

    buynow

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,14 @@
-import BuyNow from '../components/home/buyNow'
 import Layout from '../components/layout'
+import  styled from 'styled-components'
+import Pricing from '../components/home/pricing'
+import BuyNow from '../components/home/buyNow'
+
+
 export default function Home() {
   return (
     <Layout title = "Home Page">
-      <BuyNow />
+    <BuyNow />
+      <Pricing/>
       {/* USE LAYOUT IN EVERYPAGE, HAVING PROPS LIKE    title, description, tagname  just as above*/}
       {/* you can leave passing props, as Layout contains some default value set in ../components/Layout */}
     </Layout>
