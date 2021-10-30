2b6eabfa4fea51d6fd176da4c1c9487771f4524d
DevanshSrivastava19 <devanshsrivastava1911@gmail.com>
2021-10-30 16:46:03 +0530

    Added responsiveness to pricing component

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
   return (
     <Layout title = "Home Page">
       <ModiContainer>
-        Like this random styled-component, we have to render every page
+        
       </ModiContainer>
       <Pricing/>
 
